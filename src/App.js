a08ed9c837b87c8e6c068636251ba4571662b7c5
mahadihasan2 <mahadihasan125643@gmail.com>
2022-06-27 20:37:47 +0600

    navbar-done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 
 import { Route, Routes } from 'react-router-dom';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.bundle";
 import './App.css';
 import About from './Components/About/About';
 import Contact from './Components/Contact/Contact';
 import Home from './Components/Home/Home';
+import Navbar from './Components/Navbar/Navbar';
 import NotFound from './Components/NotFound/NotFound';
 import Services from './Components/Services/Services';
 
@@ -12,6 +15,7 @@ import Services from './Components/Services/Services';
 function App() {
   return (
     <div className='App'>
+      <Navbar></Navbar>
       <h1>Welcome to my React Website</h1>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
